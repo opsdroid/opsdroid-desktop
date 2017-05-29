4156b07d63af7e23ac8138bc233747727310d407
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-05-29 21:13:16 +0100

    Make builds platform specific and add zipping

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell')
 var sass = require('gulp-sass');
 var electron = require('electron-connect').server.create();
+var package_info = require('./package.json')
 
 gulp.task('serve', function () {
 
@@ -23,9 +24,17 @@ gulp.task('serve', function () {
   });
 });
 
-gulp.task('build', shell.task([
+gulp.task('build-all', shell.task([
+  'electron-packager . --overwrite --platform=all --arch=all --prune=true --out=release_builds'
+]));
+
+gulp.task('build-mac', shell.task([
   'electron-packager . --overwrite --platform=darwin --icon=assets/icons/mac/icon.icns --arch=x64 --prune=true --out=release_builds'
-]))
+]));
+
+gulp.task('zip', shell.task([
+  `zip -FSr release_builds/opsdroid-desktop-darwin-x64/opsdroid-desktop-${package_info.version}-macos-x64.zip release_builds/opsdroid-desktop-darwin-x64/opsdroid-desktop.app`
+]));
 
 gulp.task('sass', function () {
   return gulp.src('./*.scss')
