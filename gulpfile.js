cec86f595af733aa61c509269df7ed3a233355ee
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-06-09 08:03:31 +0100

    Consistent version naming

--- gulpfile.js
+++ gulpfile.js
@@ -42,10 +42,10 @@ gulp.task('build-windows', shell.task([
 ]));
 
 gulp.task('zip', shell.task([
-  `if [ -d "release_builds/opsdroid-desktop-darwin-x64" ]; then cd release_builds/opsdroid-desktop-darwin-x64 && zip -FSr opsdroid-desktop-${package_info.version}-macos-x64.zip opsdroid-desktop.app; fi`,
-  `if [ -d "release_builds/opsdroid-desktop-linux-x64" ]; then cd release_builds/opsdroid-desktop-linux-x64 && tar -cvzf opsdroid-desktop-${package_info.version}-linux-x64.tar.gz *; fi`,
-  `if [ -d "release_builds/opsdroid-desktop-linux-ia32" ]; then cd release_builds/opsdroid-desktop-linux-ia32 && tar -cvzf opsdroid-desktop-${package_info.version}-linux-i386.tar.gz *; fi`,
-  `if [ -d "release_builds/opsdroid-desktop-linux-armv7l" ]; then cd release_builds/opsdroid-desktop-linux-armv7l && tar -cvzf opsdroid-desktop-${package_info.version}-linux-armv7l.tar.gz *; fi`
+  `if [ -d "release_builds/opsdroid-desktop-darwin-x64" ]; then cd release_builds/opsdroid-desktop-darwin-x64 && zip -FSr opsdroid-desktop-v${package_info.version}-macos-x64.zip opsdroid-desktop.app; fi`,
+  `if [ -d "release_builds/opsdroid-desktop-linux-x64" ]; then cd release_builds/opsdroid-desktop-linux-x64 && tar -cvzf opsdroid-desktop-v${package_info.version}-linux-x64.tar.gz *; fi`,
+  `if [ -d "release_builds/opsdroid-desktop-linux-ia32" ]; then cd release_builds/opsdroid-desktop-linux-ia32 && tar -cvzf opsdroid-desktop-v${package_info.version}-linux-i386.tar.gz *; fi`,
+  `if [ -d "release_builds/opsdroid-desktop-linux-armv7l" ]; then cd release_builds/opsdroid-desktop-linux-armv7l && tar -cvzf opsdroid-desktop-v${package_info.version}-linux-armv7l.tar.gz *; fi`
 ]));
 
 gulp.task('sass', function () {
